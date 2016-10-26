6c86b1d93a951a7a9515e6b7d8030f3722afbf9f
LogMaker360 <firstaidabcd@gmail.com>
2016-10-26 11:56:43 +0200

    IRreceive

--- IRreciever/IRreciever.ino
+++ IRreciever/IRreciever.ino
@@ -1,22 +1,66 @@
-//code belongs to a YouTube tutorial on YouTube changel LogMaker360
-//https://www.youtube.com/watch?v=uL3eWaehEQI
+/*
+code belongs to a YouTube tutorial on YouTube changel LogMaker360
+https://www.youtube.com/watch?v=uL3eWaehEQI
+*/
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
 
-IRsend irsend;
+int counter = 0;
+int LED =13;
+int RECV_PIN = 11;
+unsigned int timeOfIncoming;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 void setup()
 {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop() {
-  if (Serial.read() != -1) {
-    for (int i = 0; i < 3; i++) {
-      irsend.sendSony(0xa90, 12); // Sony TV power code
-      delay(100);
+  if (irrecv.decode(&results)) {
+     
+     Serial.println(results.value, HEX);
+           if(results.value == 0xa90){ //if the button press equals the hex value 0xA90
+               Serial.println("counted: 1");
+               
+               timeOfIncoming = millis();
+               counter++;
+               irrecv.resume();
+               irrecv.decode(&results);
+                         
+                if(results.value == 0xa90 && timeOfIncoming+1000 > millis()){
+                 Serial.println(results.value, HEX);
+                 Serial.println("     counted: 2");
+                 
+                 counter++;
+                 irrecv.resume();
+                 irrecv.decode(&results);
+                                   
+                                    
+                                   if(results.value == 0xa90 && timeOfIncoming+1000 > millis()){
+                                  Serial.println(results.value, HEX);
+                                  Serial.println("       counted: 3");
+                                  counter++;
+                                  irrecv.resume();
+                                    }
+                }
+
+           }                        
+               
+    
+    if(counter == 3){
+      Serial.println("               TV on");
+      digitalWrite(LED, HIGH);
+      delay(4000);
+      digitalWrite(LED, LOW);
     }
+    irrecv.resume(); // Receive the next value
+    counter = 0;
+    timeOfIncoming = 0;
   }
-} 
+  }
 
